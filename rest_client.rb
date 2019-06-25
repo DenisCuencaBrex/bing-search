9654689194b23d474c7aa14b0726e3ea95e5e54a
Ebuka Umeokonkwo <ebukaume@gmail.com>
2019-06-25 16:36:07 +0100

    adds nokogiri for parsing HTML

--- rest_client.rb
+++ rest_client.rb
@@ -1,32 +1,40 @@
 require 'rest-client'
+require 'nokogiri'
 
 class BingSearch
 
-    def initialize(string_query)
-      @string_query = string_query
-    end
+  def initialize(string_query)
+    @string_query = string_query
+  end
 
-    def response
+  def response
     @response ||=  RestClient.get 'http://bing.com/search', {params: {q: @string_query}}
+  end
+
+  def process_results
+    display("Response Code: #{response.code}")
+    display("Response Cookies: \n #{response.cookies}")
+    display("Response Headers: \n #{response.headers}")
+    parse_result(response.body)
+  end
+
+  def parse_result(string)
+    display("Response Body:")
+    Nokogiri::HTML(string).css('ol h2 a').each_with_index do |node, index|
+      display("[#{index + 1}] #{node.content} \n #{node.attr('href')} \n\n")
     end
-
-    def display_results
-        puts "Response Code: #{response.code}"
-
-        puts "Response Cookies:"
-        puts "#{response.cookies}"
-
-        puts "Response Headers:"
-        puts "#{response.headers}"
-
-        puts "Response Body:"
-        puts "#{response.body}"
-    end
+  end
   
+  private
+
+  def display(string)
+    puts string
+  end
+
 end
 
 puts "Enter a search: "
 search_string = gets.chomp
 search_bing = BingSearch.new(search_string)
-search_bing.display_results
+search_bing.process_results
 
